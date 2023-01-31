8c34e575a284c2944cc3428ce8e6d1e216646ebd
shaghaygh <shqayqm145@gmail.com>
2023-01-31 17:24:31 +0330

    31/1/2023 , 17:35

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
-alert("Hello!");
-alert("Hi");
+var username = "Amir"
+var Username = "Mary"
+var userName = "Reza"
+// alert(userName);
+
+var test1 = "It is a string!";
+var age = 23;
+var bool = false;
+var n = null;
+var space = " ";
+
+var sex = "female";
+var email = null;
+var city = " ";
+
+function show() {
+    var name = "shaghayegh";
+    var family = "mohammadi";
+    alert(name +" "+family)
+}
+show();
+
+var infos = ["shaghayegh" , "mohamadi" , 23 , true , null]
+// alert (infos[2]);
+document.write("welcome"+" "+infos[0]);
+
+// or var info = new Array();
+
+
