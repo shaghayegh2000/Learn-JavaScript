fe4a7c2647fc45181fea3abe01bab0da09b92c25
shaghaygh <shqayqm145@gmail.com>
2023-02-25 21:53:43 +0330

    25/2/2023 , 18:00

---
+++ mini projects/mini project 3/third.js
@@ -0,0 +1,19 @@
+// var character = document.getElementById("character");
+// var block = document.getElementById("block");
+// var score = document.getElementById("score");
+// var counter = 0;
+
+function jump(){
+    character.classList.add("animation");
+    setTimeout(function() {
+        character.classList.remove("animation");
+    }, 500);
+}
+
+// setInterval(function(){
+//     let blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+//     let chartop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//     if(blockleft<65 && blockleft>50 && chartop>=179){
+//         alert("!Game Over!");
+//     }
+// }, 10);
