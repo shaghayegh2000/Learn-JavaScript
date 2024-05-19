fc1366131d1d4992f24d9da1c669ee6c0aa57dd6
shaghaygh <shqayqm145@gmail.com>
2024-05-19 15:37:37 +0330

    19/5/2024

---
+++ blur loading/script.js
@@ -0,0 +1,21 @@
+const loadText = document.querySelector('.text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+function scale(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+let blur = setInterval(() => {
+    load++;
+
+    load > 99 ? clearInterval(blur) : "";
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0 , 100 , 1 , 0)
+        
+    // console.log(load)
+
+}, 30);
+
