264c8ee0ccfd5ddfff0c67ca55aa8f38515fa1c9
shaghaygh <shqayqm145@gmail.com>
2024-06-06 21:51:19 +0330

    6/6/2024

---
+++ quizApp/script.js
@@ -0,0 +1,60 @@
+const quizDatas = [
+    {
+        question: "What is the most used programming language in 2019?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "Who is the President of US?",
+        a: "Florin Pop",
+        b: "Donald Trump",
+        c: "Ivan Saldano",
+        d: "Mihai Andrei",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const a_text = document.querySelector('#a_text');
+const b_text = document.querySelector('#b_text');
+const c_text = document.querySelector('#c_text');
+const d_text = document.querySelector('#d_text');
+const question = document.querySelector('#question');
+const submit = document.querySelector('#submit');
+
+let cuurenQuiz = 0;
+
+const loadQuiz = () => {
+    const currentQuizData = quizDatas[cuurenQuiz];
+    question.innerHTML = currentQuizData.question;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+
+    cuurenQuiz++;
+}
+
+loadQuiz();
+
+submit.addEventListener('click', () => {
+    cuurenQuiz < quizDatas.length ? loadQuiz() : alert('done') ;
+})
