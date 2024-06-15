1159045d495361a14b4e4c0913531f2fb37510f9
shaghaygh <shqayqm145@gmail.com>
2024-06-15 12:54:45 +0330

    15/6/2023

--- Alibaba/scripts/script.js
+++ Alibaba/scripts/script.js
@@ -1,20 +1,17 @@
 const parvazMenu = document.querySelector('.parvaz-menu'),
     moreBtn = document.querySelector('.more'),
     moreList = document.querySelector('.header-navbar-item7'),
-    menuBox = document.querySelector('.header-navbar-item1');
+    menuBox = document.querySelector('.header-navbar-item1'),
+    html = document.querySelector('html');
 
 moreList.addEventListener('click', (e) => {
     e.preventDefault();
     moreBtn.classList.toggle('display');
+    parvazMenu.classList.remove('display');
 })
 
 menuBox.addEventListener('click', (e) => {
     e.preventDefault();
     parvazMenu.classList.toggle('display');
+    moreBtn.classList.remove('display');
 })
-
-if (moreBtn.classList == display || parvazMenu.classList == display) {
-    window.addEventListener('click', () => {
-        moreBtn.classList.remove('display')
-    })
-}
