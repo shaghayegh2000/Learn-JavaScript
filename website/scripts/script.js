bba4a4be0fd3f929a07a8b9083b3d7dee78df928
shaghaygh <shqayqm145@gmail.com>
2024-06-16 19:20:44 +0330

    16/6/2024

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('.mobile-nav'),
 navMenuBtn = document.querySelector('.nav-menu-btn'),
-navCloseBtn = document.querySelector('.nav-close-btn');
+navCloseBtn = document.querySelector('.nav-close-btn') ,
+themeBtn = document.querySelectorAll(".theme-btn");
 
 navMenuBtn.addEventListener('click', () => {
     nav.classList.toggle('active')
@@ -9,3 +10,15 @@ navMenuBtn.addEventListener('click', () => {
 navCloseBtn.addEventListener('click', () => {
     nav.classList.toggle('active')
 })
+
+for (let i = 0; i < themeBtn.length; i++) {
+  themeBtn[i].addEventListener("click", function () {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+
+    for (let j = 0; j < themeBtn.length; j++) {
+      themeBtn[j].classList.toggle("light");
+      themeBtn[j].classList.toggle("dark");
+    }
+  });
+}
